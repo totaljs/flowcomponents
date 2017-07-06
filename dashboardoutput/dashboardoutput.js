8f3e9f2075d2398eb977eaa03c1eb0b538e2535b
Peter Širka <petersirka@gmail.com>
2017-07-06 14:13:29 +0200

    Add new component.

---
+++ dashboardoutput/dashboardoutput.js
@@ -0,0 +1,29 @@
+exports.id = 'dashboardoutput';
+exports.title = 'Output';
+exports.version = '1.0.0';
+exports.author = 'Peter Širka';
+exports.group = 'Dashboard';
+exports.color = '#5CB36D';
+exports.input = true;
+exports.output = 0;
+exports.readme = `# Dashboard Output
+
+This component shows the content as it is in Total.js Dashboard.`;
+
+exports.install = function(instance) {
+
+	var lastdata = null;
+
+	instance.on('data', function(response) {
+		lastdata = response.data;
+		lastdata != null && instance.dashboard('laststate', lastdata);
+	});
+
+	instance.on('dashboard', function(type) {
+		switch (type) {
+			case 'laststate':
+				lastdata != null && instance.dashboard(type, lastdata);
+				break;
+		}
+	});
+};
---
+++ dashboardoutput/readme.md
@@ -0,0 +1,3 @@
+# Dashboard Output
+
+This component shows the content as it is in Total.js Dashboard.
