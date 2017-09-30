e10a1b0a981ec6502c85b84e6a908cd1dc32bff8
Martin Smola <smola.martin@gmail.com>
2017-09-30 16:31:12 +0200

    Improved flowboardsimplethermostat

--- flowboardsimplethermostat/flowboardsimplethermostat.js
+++ flowboardsimplethermostat/flowboardsimplethermostat.js
@@ -4,16 +4,19 @@ exports.group = 'Flowbard';
 exports.color = '#AC92EC';
 exports.icon = 'fa-thermometer-half';
 exports.click = true;
-exports.input = 2;
-exports.output = ['red', 'blue'];
+exports.input = ['green', 'blue', 'yellow', 'black', 'grey'];
+exports.output = ['red', 'lightgrey' , 'blue'];
 exports.version = '1.0.0';
 exports.author = 'Martin Smola';
-exports.options = { temp_required: 21, hysteresis: 0.5, temp_current: 21, heating: false, enabled: true };
+exports.options = { temp_heating_day: 21, temp_heating_night: 21, temp_heating_away: 21, hysteresis: 0.5, temp_current: 21, heating: false, cooling: false, enabled: true };
 exports.readme = `# Flowbard: Simple thermostat
 
 ### Inputs
 - First input can be used to enable/disable the thermostat. Disabling will also send 'true' to second output.
-- Second input is default input for data.
+- Second input is for current temperature.
+- Third input is to set Day mode.
+- Fourth input is to set Night mode.
+- Fifth input is to set Away mode.
 
 ### Outputs
 - Data are send to first output when the heating should start and second to stop the heating.`;
@@ -25,8 +28,14 @@ exports.html = `<div class="padding">
 		</div>
 	</div>	
 	<div class="row">
-		<div class="col-md-6 m">
-			<div data-jc="textbox" data-jc-path="temp_required" data-jc-config="placeholder:21;type:number;increment:true" class="m">Required temperature &#8451;</div>
+		<div class="col-md-4 m">
+			<div data-jc="textbox" data-jc-path="temp_heating_day" data-jc-config="placeholder:22;type:number;increment:true" class="m">Heating: day temperature &#8451;</div>
+		</div>
+		<div class="col-md-4 m">
+			<div data-jc="textbox" data-jc-path="temp_heating_night" data-jc-config="placeholder:21;type:number;increment:true" class="m">Heating: night temperature &#8451;</div>
+		</div>
+		<div class="col-md-4 m">
+			<div data-jc="textbox" data-jc-path="temp_heating_away" data-jc-config="placeholder:20;type:number;increment:true" class="m">Heating: away temperature &#8451;</div>
 		</div>
 	</div>	
 	<div class="row">
@@ -41,20 +50,18 @@ exports.install = function(instance) {
 
 
 	instance.custom.reconfigure = function() {
-		var options = instance.options;
-
-		instance.options = U.extend({ temp_required: 21, hysteresis: 0.5, temp_current: 21, enabled: true }, instance.options, true);		
-		send(instance.options);
-		instance.custom.status();
+		instance.options = U.extend({ temp_heating_day: 22, temp_heating_night: 21, temp_heating_away: 20, hysteresis: 0.5, temp_current: 21, enabled: false, heating: false, mode: 'away', name: instance.name }, instance.options, true);		
+		send();
 	};
 
 
 	instance.custom.status = function() {
-		var options = instance.options;
-		instance.status(global.FLOWBOARD ? '{0} +-{1} {2}'.format(options.temp_required, options.hysteresis, instance.options.enabled ? 'Enabled' : 'Disabled') : 'Flowbard not found.', global.FLOWBOARD ? null : 'red');
-		instance.flowboard('options', instance.options);
+		var options = instance.options;	
+		instance.status(global.FLOWBOARD ? '{0} | +-{1} | {2} | {3}'.format(options['temp_heating_'+ options.mode] || '??', options.hysteresis, options.enabled ? 'enabled' : 'disabled', options.mode) : 'Flowbard not found.', global.FLOWBOARD ? null : 'red');
+		instance.flowboard('options', options);
 	};
 
+	// enable/disable
 	instance.on('0', function(flowdata) {
 		var temp = flowdata.data;
 		var options = instance.options;
@@ -63,24 +70,14 @@ exports.install = function(instance) {
 			options.enabled = true;
 		else
 			options.enabled = false;
-
-		instance.custom.status();
+	
+		send();
 	});
 
+	// temperature
 	instance.on('1', function(flowdata) {
-		var val;
 		var options = instance.options;
-
-		if (options.property) {
-			if (options.property.indexOf('.') === -1)
-				options.temp_current = flowdata.data[options.property];
-			else
-				options.temp_current = U.get(flowdata.data, options.property);
-		} else {
-			options.temp_current = flowdata.data;
-		}
-
-		val = options.temp_current;
+		var val = options.temp_current = getVal(flowdata.data, options.property);
 
 		if (!val)
 			return;
@@ -91,38 +88,83 @@ exports.install = function(instance) {
 				instance.error('Error, input value is not a number: ' + val);
 				return;
 			}
-		}
-
-		send(options);
+		}	
+	
+		send();
 	});
 
-	function send(options) {
+	// set day mode
+	instance.on('2', function(flowdata) {
+		var options = instance.options;
+		var m = flowdata.data;
+
+		if (!m)
+			options.mode = 'day';
+		// if data is a string with one of the supported modes then set it as current mode
+		else if (m === 'day' || m === 'night' || m === 'away')
+			options.mode = m;
+		// if data is object with mode property then set it as current mode
+		else if (m.mode && (m.mode === 'day' || m.mode === 'night' || m.mode === 'away'))
+			options.mode = m.mode;
+		else // default mode is 'day'
+			options.mode = 'day';
+
+		send();
+	});
+
+	// set night mode
+	instance.on('3', function(flowdata) {
+		instance.options.mode = 'night';
+		send();
+	});
+
+	// set away mode
+	instance.on('4', function(flowdata) {
+		instance.options.mode = 'away';
+		send();
+	});
+
+	function send() {
+		var options = instance.options;
+
 		if (!options.enabled) {			
 			options.heating = false;
 			instance.send(1, true);
-			return;
+		} else {
+			if (options.temp_current < (options['temp_heating_' + options.mode] - options.hysteresis)) {
+				// start
+				options.heating = true;
+				instance.send(0, true);
+			} else if (options.temp_current > (options['temp_heating_' + options.mode] + options.hysteresis)) {
+				// stop
+				options.heating = false;
+				instance.send(1, true);
+			}
+		}	
+
+		instance.custom.status();
+	};
+
+	function getVal(obj, path) {
+		if (path) {
+			if (path.indexOf('.') === -1)
+				return obj[path];
+			else
+				return U.get(obj, path);
 		}
 
-		if (options.temp_current < (options.temp_required - options.hysteresis)) {
-			// start
-			options.heating = true;
-			instance.send(0, true);
-		} else if (options.temp_current > (options.temp_required + options.hysteresis)) {
-			// stop
-			options.heating = false;
-			instance.send(1, true);
-		}
-		
-		instance.flowboard('options', options);			
+		return obj;
 	};
 
 	instance.on('options', instance.custom.reconfigure);
-	instance.custom.reconfigure();
+	setTimeout(instance.custom.reconfigure, 3000);
 
 	instance.on('flowboard', function(type, data) {
 		switch (type) {
 			case 'setoptions':
-				instance.options.temp_required = data.temp_required;
+				instance.options.temp_heating_day = data.temp_heating_day;
+				instance.options.temp_heating_night = data.temp_heating_night;
+				instance.options.temp_heating_away = data.temp_heating_away;
 				instance.options.hysteresis = data.hysteresis;
 				instance.custom.reconfigure();
 				// send options to designer
@@ -130,13 +172,13 @@ exports.install = function(instance) {
 				break;
 
 			case 'getoptions':
-				instance.flowboard('options', instance.options);
+				instance.flowboard('options', options);
 				break;
 		}
 	});
 
 	instance.on('click', function() {
 		instance.options.enabled = !instance.options.enabled;
-		instance.custom.status();
+		send();
 	});
 };
