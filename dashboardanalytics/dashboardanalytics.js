b5492c04281fbebf6a72dec013576a1ad7838a3e
Martin Smola <smola.martin@gmail.com>
2017-11-10 22:45:06 +0100

    Updated dashboardanalytics dropdown items

--- dashboardanalytics/dashboardanalytics.js
+++ dashboardanalytics/dashboardanalytics.js
@@ -29,7 +29,7 @@ This components sends to Dashboard two types of data:
 - \`stats\` with stats`;
 
 exports.html = `<div class="padding">
-	<div data-jc="dropdown" data-jc-path="type" class="m" data-jc-config="items:,@(Hourly\: Sum values)|sum,@(Hourly\: A maximum value)|max,@(Hourly\: A minimum value)|min,@(Hourly\: An average value)|avg,@(Hourly\: An average (median) value)|median,@(Daily\: Sum values)|Dsum,@(Daily\: A maximum value)|Dmax,@(Daily\: A minimum value)|Dmin,@(Daily\: An average value)|Davg,@(Daily\: An average (median) value)|Dmedian;required:true">@(Type)</div>
+	<div data-jc="dropdown" data-jc-path="type" class="m" data-jc-config="items:,@(Hourly - Sum values)|sum,@(Hourly - A maximum value)|max,@(Hourly - A minimum value)|min,@(Hourly - An average value)|avg,@(Hourly - An average (median) value)|median,@(Daily - Sum values)|Dsum,@(Daily - A maximum value)|Dmax,@(Daily - A minimum value)|Dmin,@(Daily - An average value)|Davg,@(Daily - An average (median) value)|Dmedian;required:true">@(Type)</div>
 	<div data-jc="codemirror" data-jc-path="fn" data-jc-config="type:javascript" class="m">@(Analyzator)</div>
 	<div class="row">
 		<div class="col-md-3 m">
