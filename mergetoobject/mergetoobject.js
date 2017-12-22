a564a8d77660b847a99bee7ca9d5f1a6823692c5
Peter Širka <petersirka@gmail.com>
2017-12-22 21:13:56 +0100

    Add new option.

--- mergetoobject/mergetoobject.js
+++ mergetoobject/mergetoobject.js
@@ -5,13 +5,15 @@ exports.color = '#656D78';
 exports.input = 2;
 exports.click = true;
 exports.output = 1;
-exports.options = { props: [] };
+exports.options = { props: [], id: true };
 exports.author = 'Martin Smola';
 exports.icon = 'compress';
 
 exports.html = `<div class="padding">
 	<div data-jc="textboxlist" data-jc-path="props" data-jc-config="maxlength:50;placeholder:@(Type a property name and press enter);icon:list">@(Properties)</div>
 	<div class="help">@(Data comming to each of the inputs will be assign to a property from top to bottom. The first input to the first property.)</div>
+	<hr />
+	<div data-jc="checkbox" data-jc-path="id" class="m">@(Merge data by same FlowData identificator)</div>
 	<script>
 		ON('save.mergetoobject', function(component, options) {
 			if (options.props && options.props.length)
@@ -31,21 +33,22 @@ exports.install = function(instance) {
 
 	instance.on('data', function(response) {
 
-		var id = response.id;
+		var id = instance.options.id ? response.id : '$';
 
-		data[id] = data[id] || {};
+		!data[id] && (data[id] = {});
+
 		var prop = instance.options.props[response.index];
-		if (!prop)
-			instance.debug('No property name for current input:', response.index);
-		else
+		if (prop)
 			data[id][prop] = response.data;
+		else
+			instance.debug('No property name for current input:', response.index);
 
 		instance.status(Object.keys(data[id]).join(', '), 'red');
 		if (Object.keys(data[id]).length === instance.options.props.length) {
 			response.data = data[id];
 			instance.send2(response);
 			setTimeout2(instance.id, () => instance.status(''), 500, 10);
-			data[id] = {};
+			data[id] = null;
 		}
 	});
 
