f23d5e9de7074b70e5a97d94fe402c7ea606f7a6
Martin Smola <smola.martin@gmail.com>
2017-12-26 18:43:13 +0100

    Updated mosca again

--- mosca/mosca.js
+++ mosca/mosca.js
@@ -1,95 +1,110 @@
 exports.id = 'mqttserver';
 exports.title = 'MQTT server (Mosca)';
 exports.group = 'MQTT';
-exports.color = '#656D78';
+exports.color = '#888600';
 exports.version = '1.0.0';
 exports.icon = 'clock-o';
 exports.input = false;
 exports.output = ['green', 'red', 'white'];
 exports.author = 'Martin Smola';
-exports.options = {};
+exports.options = { host: '127.0.0.1', port: 1883, ws: false, portws: 1884 };
 exports.traffic = false;
 exports.npm = ['mosca'];
 
 exports.html = `<div class="padding">
-	<section>
-		<label><i class="fa fa-exchange"></i>@(MQTT server (Mosca))</label>
-		<div class="padding npb">
-			<div class="row">
-				<!--<div class="col-md-6">
-					<div data-jc="textbox" data-jc-path="host" data-jc-config="placeholder:127.0.0.1" class="m">Hostname or IP address</div>
-				</div>-->
-				<div class="col-md-6">
-					<div data-jc="textbox" data-jc-path="port" data-jc-config="placeholder:1883" class="m">Port (1883 by default)</div>
-				</div>
-			</div>
+	<div class="row">
+		<!--<div class="col-md-6">
+			<div data-jc="textbox" data-jc-path="host" data-jc-config="placeholder:127.0.0.1;disabled:true" class="m">Hostname or IP address</div>
+		</div>-->
+		<div class="col-md-6">
+			<div data-jc="textbox" data-jc-path="port" data-jc-config="placeholder:1883" class="m">Port (1883 by default)</div>
 		</div>
-		<div class="padding npb">
-			<div class="row">
-				<div class="col-md-6">
-					<div data-jc="checkbox" data-jc-path="ws" data-jc-config="placeholder:127.0.0.1" class="m">Enable mqqt over websockets?</div>
-				</div>
-				<div class="col-md-6">
-					<div data-jc="textbox" data-jc-path="portws" data-jc-config="placeholder:1884" class="m">Port (1884 by default)</div>
-				</div>
-			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div data-jc="checkbox" data-jc-path="ws" data-jc-config="placeholder:127.0.0.1" class="m">Enable mqqt over websockets?</div>
 		</div>
-	</section>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div data-jc="textbox" data-jc-path="portws" data-jc-config="placeholder:1884" class="m">Websocket port (1884 by default)</div>
+		</div>
+	</div>
 </div>`;
 
 exports.readme = `
 # MQTT Broker
 
 Uses Mosca https://github.com/mcollina/mosca
+
+## Outputs
+- (green) new client connected --> (object) client
+- (red) client disconnected    --> (object) client
+- (white) new message          --> (object) { packet, client }
 `;
 
-var mosca;
-
 exports.install = function(instance) {
-	mosca = require('mosca');
+	var mosca = require('mosca');
 	var server;
-
-	instance.custom.reconfigure = function(options, old_options) {
-
-		if (server && options !== old_options)
-			server.close(() => instance.custom.start_server());
-		else
-			instance.custom.start_server();
-	};
-
-	instance.on('close', server.close);
-	instance.on('options', instance.custom.reconfigure);
-
-	instance.custom.reconfigure();
 
 	instance.custom.start_server = function(){
 		var options = instance.options;
 
-		var settings = { port: options.port || 1883, persistence: mosca.persistence.Memory };
+		var settings = { 
+			host: options.host,
+			port: options.port || 1883, 
+			interfaces: [{
+					type: 'mqtt',
+					port: options.port || 1883
+				}]
+		};
+
 		if (options.ws)
-			settings.http = {
-				http: {
-    				port: options.portws || 1884,
-    				bundle: true
-    			}
-			};
+			settings.interfaces.push({
+				type: 'http',
+				port: options.portws || 1884
+			});
+
 		var server = new mosca.Server(settings);
 
 		server.on('ready', function() {
-			instance.status('Running...');
+			var status = 'mgtt {0}:{1}'.format(options.host, options.port);
+			if (options.ws)
+				status += ' | ws port {0}'.format(options.portws);
+			instance.status(status);
 		});
 
 		server.on('clientConnected', function(client) {
-			instance.send2(1, client);
+			instance.send2(1, {
+				id: client.id
+			});
 		});
 
 		server.on('clientDisconnected', function(client) {
-			instance.send2(2, client);
+			instance.send2(2, {
+				id: client.id
+			});
 		});
 
 		// fired when a message is received
-		server.on('published', function(packet, client) {
-			instance.send2(3, packet, client);
+		server.on('published', function(packet) {
+			instance.send2(3, packet);
 		});
 	};
+
+	instance.custom.reconfigure = function(options, old_options) {
+
+		if (server && options !== old_options)
+			server.close(() => setTimeout(instance.custom.start_server, 100));
+		else
+			instance.custom.start_server();
+	};
+
+	instance.on('close', function(){
+		server && server.close();
+	});
+
+	instance.on('options', instance.custom.reconfigure);
+
+	instance.custom.reconfigure();
 };
