735307b4ef292e174c9c96785684e9dea1bdc864
Peter Širka <petersirka@gmail.com>
2018-01-18 11:12:10 +0100

    Add new features.

--- email/email.js
+++ email/email.js
@@ -7,12 +7,13 @@ exports.author = 'Peter Širka';
 exports.version = '1.2.0';
 exports.output = ['green', 'red'];
 exports.icon = 'envelope-o';
-exports.options = { errors: true };
+exports.options = { errors: true, type: 'smtp' };
 
 exports.html = `<div class="padding">
-	<section>
+	<div data-jc="dropdown" data-jc-path="type" data-jc-config="required:true;items:@(Internal SMTP)|internal,@(Custom defined SMTP)|smtp" data-jc-value="'smtp'" class="m">@(Sender)</div>
+	<section data-b="?.type" data-b-visible="value === 'smtp'" class="hidden m">
 		<label><i class="fa fa-lock"></i>@(SMTP sender)</label>
-		<div class="padding">
+		<div class="padding bg-yellow">
 			<div class="row">
 				<div class="col-md-6 m">
 					<div data-jc="textbox" data-jc-path="smtp" data-jc-config="required:true;maxlength:50">@(SMTP server)</div>
@@ -29,14 +30,14 @@ exports.html = `<div class="padding">
 					<div data-jc="textbox" data-jc-path="password" data-jc-config="maxlength:50;placeholder="@(SMTP password)">@(Password)</div>
 				</div>
 			</div>
-			<div data-jc="checkbox" data-jc-path="errors">@(Enable internal error handling)</div>
 		</div>
 	</section>
+	<div data-jc="checkbox" data-jc-path="errors">@(Enable internal error handling)</div>
 	<br />
 	<section>
 		<label><i class="fa fa-envelope"></i>@(Mail message settings)</label>
-		<div class="padding npb">
-			<div class="b">@(Message)</div>
+		<div class="padding">
+			<div data-jc="textbox" data-jc-path="subject" class="m" data-jc-config="required:true;maxlength:100" data-jc-value="''">@(Subject)</div>
 			<div class="row mt10">
 				<div class="col-md-6 m">
 					<div data-jc="textbox" data-jc-path="from" data-jc-config="required:true;maxlength:120;icon:envelope-o" data-jc-value="'@'">@(From)</div>
@@ -45,10 +46,35 @@ exports.html = `<div class="padding">
 					<div data-jc="textbox" data-jc-path="target" data-jc-config="required:true;maxlength:120;icon:envelope-o" data-jc-value="'@'">@(To)</div>
 				</div>
 			</div>
-			<div data-jc="textbox" data-jc-path="subject" class="m" data-jc-config="required:true;maxlength:100" data-jc-value="'@'">@(Subject)</div>
+			<div class="row">
+				<div class="col-md-6 m">
+					<div data-jc="textbox" data-jc-path="cc" data-jc-config="maxlength:120;icon:envelope-o" data-jc-value="''">@(CC)</div>
+				</div>
+				<div class="col-md-6 m">
+					<div data-jc="textbox" data-jc-path="bbcc" data-jc-config="maxlength:120;icon:envelope-o" data-jc-value="''">@(BCC)</div>
+				</div>
+			</div>
 		</div>
 	</section>
-</div>`;
+</div>
+<script>
+	ON('save.email', function(component, options) {
+		var builder = [];
+		builder.push('### @(Configuration)');
+		builder.push('');
+		builder.push('- SMTP: __' + (options.type === 'smtp' ? options.smtp : '@(Internal email)') + '__');
+		builder.push('- @(authorization): __' + (options.user && options.password ? '@(yes)' : '@(no)') + '__');
+		builder.push('- @(internal error handling): __' + (options.errors ? '@(yes)' : '@(no)') + '__');
+		builder.push('---');
+		builder.push('- @(from): __' + options.from + '__');
+		builder.push('- @(to): __' + options.target + '__');
+		builder.push('- @(cc): __' + (options.cc || '@(none)') + '__');
+		builder.push('- @(bcc): __' + (options.bcc || '@(none)') + '__');
+		builder.push('---');
+		builder.push('- @(subject): __' + options.subject + '__');
+		component.notes = builder.join('\\n');
+	});
+</script>`;
 
 exports.readme = `# Email sender
 
@@ -68,6 +94,9 @@ Are performed via FlowData repository and can be used for subject, from/to addre
 __Attachments__:
 \`FlowData\` repository needs to contain \`attachments\` key with user-defined array in the form:
 
+__Multile addresses__:
+This component can send email to multiple addresses but the addresses need to be declared in FlowData repository as \`String Array\`.
+
 \`\`\`javascript
 [
 	{ filename: '/absolute/path/to/some/file.pdf', name: 'report.pdf' },
@@ -86,9 +115,29 @@ exports.install = function(instance) {
 	instance.custom.send = function(body, msg) {
 		var options = instance.options;
 		var message = Mail.create(msg.arg(options.subject), typeof(body) === 'object' ? JSON.stringify(body) : body.toString());
+
 		message.from(msg.arg(options.from));
-		message.to(msg.arg(options.target));
-		message.send(options.smtp, smtp);
+
+		var target = msg.arg(options.target);
+		if (target instanceof Array) {
+			for (var i = 0; i < target.length; i++)
+				message.to(target[i]);
+		} else if (target)
+			message.to(target);
+
+		target = msg.arg(options.cc);
+		if (target instanceof Array) {
+			for (var i = 0; i < target.length; i++)
+				message.cc(target[i]);
+		} else if (target)
+			message.cc(target);
+
+		target = msg.arg(options.bcc);
+		if (target instanceof Array) {
+			for (var i = 0; i < target.length; i++)
+				message.bcc(target[i]);
+		} else if (target)
+			message.bcc(target);
 
 		var a = msg.get('attachments');
 		if (a instanceof Array) {
@@ -104,6 +153,11 @@ exports.install = function(instance) {
 			} else
 				instance.send2(0, msg);
 		});
+
+		if (options.internal)
+			message.send2();
+		else
+			message.send(options.smtp, smtp);
 	};
 
 	instance.reconfigure = function() {
@@ -116,22 +170,27 @@ exports.install = function(instance) {
 			return;
 		}
 
-		smtp = {};
-		options.user && (smtp.user = options.user);
-		options.password && (smtp.password = options.password);
-		options.port && (smtp.port = options.port);
+		if (options.type === 'smtp') {
+			smtp = {};
+			options.user && (smtp.user = options.user);
+			options.password && (smtp.password = options.password);
+			options.port && (smtp.port = options.port);
 
-		if (smtp.port !== 25)
-			smtp.secure = true;
+			if (smtp.port !== 25)
+				smtp.secure = true;
 
-		Mail.try(options.smtp, smtp, function(err) {
-			if (err) {
-				Mail.try(options.smtp, smtp, function(err) {
-					instance.status(err ? err.toString() : '', err ? 'red' : undefined);
-				});
-			} else
-				instance.status('');
-		});
+			Mail.try(options.smtp, smtp, function(err) {
+				if (err) {
+					Mail.try(options.smtp, smtp, function(err) {
+						instance.status(err ? err.toString() : '', err ? 'red' : undefined);
+					});
+				} else
+					instance.status('');
+			});
+		} else {
+			instance.status('');
+			smtp = null;
+		}
 	};
 
 	instance.on('options', instance.reconfigure);
