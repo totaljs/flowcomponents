0690d2bfd26dc98f5608664de03175c7b597ca1b
Peter Širka <petersirka@gmail.com>
2018-01-19 21:51:12 +0100

    Update component.

--- restinterpreter/restinterpreter.js
+++ restinterpreter/restinterpreter.js
@@ -1,18 +1,29 @@
 exports.id = 'restinterpreter';
 exports.title = 'Interpreter';
 exports.group = 'REST';
-exports.color = '#B0331B';
+exports.color = '#666D77';
 exports.input = false;
 exports.output = 1;
 exports.author = 'Peter Širka';
 exports.icon = 'globe';
 exports.version = '1.0.0';
 exports.options = { channel: '' };
-exports.html = '';
+exports.html = `<div class="padding">
+	<div class="row">
+		<div class="col-md-6">
+			<div data-jc="textbox" data-jc-path="channel" data-jc-config="required:true;maxlength:40;placeholder:@(e.g. import)">@(Channel name)</div>
+			<div class="help">@(Type a channel name.)</div>
+		</div>
+	</div>
+</div>`;
 
 exports.readme = `# REST: Interpreter
 
-The component is input component and it listens on a specific \`channel\`. It registers a global method \`INTERPRET(channel, data)\` and developer needs to execute this method directly in the code. The component can interpret some events or behaviour of Total.js application from code directly.`;
+The component is an input component and it listens on a specific \`channel\`. The component can interpret some events or behaviour of Total.js application from code directly.
+
+---
+
+This component registers a global method \`INTERPRET(channel, data)\` and developer needs to execute this method directly in the code.`;
 
 var channels = {};
 
@@ -45,12 +56,14 @@ exports.install = function(instance) {
 		channel && instance.options.channel !== channel && remove(channel);
 		channel = instance.options.channel;
 		if (channel) {
+
 			if (channels[channel]) {
 				if (channels[channel].indexOf(instance) === -1)
 					channels[channel].push(instance);
 			} else
 				channels[channel] = [instance];
-			instance.status('Listen: ' + channel);
+
+			instance.status('Channel: ' + channel);
 		} else
 			instance.status('Not configured', 'red');
 	};
