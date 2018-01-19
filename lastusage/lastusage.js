c9a37d389df0ab45aa839d8f6dcb19f2639fb519
Peter Širka <petersirka@gmail.com>
2018-01-19 11:44:33 +0100

    Add `persistent` storage for `lastusage` component.

--- lastusage/lastusage.js
+++ lastusage/lastusage.js
@@ -1,6 +1,6 @@
 exports.id = 'lastusage';
 exports.title = 'Last Usage';
-exports.version = '1.0.0';
+exports.version = '1.1.0';
 exports.author = 'Peter Širka';
 exports.color = '#656D78';
 exports.input = true;
@@ -9,7 +9,7 @@ exports.options = { format: 'dd.MM.yyyy HH:mm:ss' };
 exports.traffic = false;
 exports.readme = `# Last Usage
 
-This component remembers date and time of last usage.`;
+This component remembers date and time of last received data. The component keeps stats of usage in NoSQL embedded DB.`;
 
 exports.html = `<div class="padding">
 	<div class="row">
@@ -24,13 +24,29 @@ exports.install = function(instance) {
 	var lastusage = null;
 
 	instance.on('data', function() {
+
 		lastusage = new Date();
 		instance.custom.status();
+
+		// Internal stats
+		NOSQL('flowlastusage').counter.hit(instance.id, 1);
 	});
 
 	instance.custom.status = function() {
-		lastusage && setTimeout2(instance.id, () => instance.status(lastusage.format(instance.options.format)), 100);
+		lastusage && setTimeout2(instance.id, function() {
+			var data = {};
+			data.id = instance.id;
+			data.date = lastusage;
+			data.name = instance.name;
+			NOSQL('flowlastusage').update(data, data).where('id', data.id);
+			instance.status(lastusage.format(instance.options.format));
+		}, 1000);
 	};
 
 	instance.on('options', instance.custom.status);
+
+	NOSQL('flowlastusage').one().where('id', instance.id).callback(function(err, response) {
+		if (response)
+			lastusage = response.date;
+	});
 };
--- lastusage/readme.md
+++ lastusage/readme.md
@@ -1,3 +1,3 @@
 # Last Usage
 
-This component remembers date and time of last usage.
+This component remembers date and time of last received data. The component keeps stats of usage in NoSQL embedded DB.
