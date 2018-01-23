3e3aa7aa0dfc88d142d8a08076e3a0662be22be1
Peter Širka <petersirka@gmail.com>
2018-01-23 13:31:14 +0100

    Add duration.

--- httpresponse/httpresponse.js
+++ httpresponse/httpresponse.js
@@ -4,27 +4,28 @@ exports.group = 'HTTP';
 exports.color = '#5D9CEC';
 exports.icon = 'arrow-right';
 exports.input = true;
-exports.output = 0;
-exports.version = '1.0.0';
+exports.output = ['#666D76'];
+exports.version = '2.0.0';
 exports.author = 'Martin Smola';
 exports.readme = `# HTTP response
 
-HTTP response will respond with data recieved using data-type set in Settings form or plain text if not set.`;
+HTTP response will respond with data recieved using data-type set in Settings form or plain text if not set. Output is the message duration \`Number\` in seconds.`;
 
 exports.html = `<div class="padding">
-	<div class="row">
-		<div class="col-md-6 m">
-			<div data-jc="dropdown" data-jc-path="datatype" data-jc-config="required:true;items:,Empty response|emptyresponse,JSON|json,HTML|html,Plain text|plain,XML|xml">@(Response data-type (json by default))</div>
-		</div>
-	</div>
+	<div data-jc="dropdown" data-jc-path="datatype" data-jc-config="required:true;items:,Empty response|emptyresponse,JSON|json,HTML|html,Plain text|plain,XML|xml">@(Response data-type)</div>
+	<div class="help"><code>JSON</code> is by default.</div>
 </div>`;
 
 const ERRORMESSAGE = {};
 
 exports.install = function(instance) {
 
-	instance.on('data', function(flowdata){
-		var ctrl = flowdata.get('controller');
+	var dursum = 0;
+	var durcount = 0;
+
+	instance.on('data', function(flowdata) {
+
+		var ctrl = flowdata.repository.controller;
 		var data = flowdata.data;
 
 		if (!ctrl) {
@@ -34,8 +35,13 @@ exports.install = function(instance) {
 			return;
 		}
 
-		var datatype = instance.options.datatype || 'json';
+		durcount++;
+		dursum += ((new Date() - flowdata.begin) / 1000).floor(2);
+		setTimeout2(instance.id, instance.custom.duration, 500, 10);
 
+		ctrl.$flowdata = flowdata;
+
+		var datatype = instance.options.datatype || 'json';
 		if (datatype === 'emptyresponse')
 			return ctrl.plain('');
 
@@ -60,4 +66,15 @@ exports.install = function(instance) {
 				break;
 		}
 	});
+
+	instance.on('service', function() {
+		dursum = 0;
+		durcount = 0;
+	});
+
+	instance.custom.duration = function() {
+		var avg = (dursum / durcount).floor(2);
+		instance.status(avg + ' sec.');
+		instance.send2(0, avg);
+	};
 };
--- restresponse/restresponse.js
+++ restresponse/restresponse.js
@@ -3,7 +3,7 @@ exports.title = 'Response';
 exports.group = 'REST';
 exports.color = '#B0331B';
 exports.input = 1;
-exports.output = false;
+exports.output = ['#666D76'];
 exports.author = 'Peter Širka';
 exports.icon = 'globe';
 exports.version = '1.0.0';
@@ -12,10 +12,32 @@ exports.html = '';
 
 exports.readme = `# REST: Response
 
-This component responds with JSON.`;
+This component responds with JSON automatically. Output is the message duration \`Number\` in seconds.`;
 
 exports.install = function(instance) {
-	instance.on('data', function(response){
-		response.repository.controller && response.repository.controller.json(response.data);
+
+	var dursum = 0;
+	var durcount = 0;
+
+	instance.on('data', function(response) {
+		if (response.repository.controller) {
+			var ctrl = response.repository.controller;
+			ctrl.$flowdata = response;
+			ctrl.json(response.data);
+			durcount++;
+			dursum += ((new Date() - response.begin) / 1000).floor(2);
+			setTimeout2(instance.id, instance.custom.duration, 500, 10);
+		}
 	});
+
+	instance.on('service', function() {
+		dursum = 0;
+		durcount = 0;
+	});
+
+	instance.custom.duration = function() {
+		var avg = (dursum / durcount).floor(2);
+		instance.status(avg + ' sec.');
+		instance.send2(0, avg);
+	};
 };
