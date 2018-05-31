58d7ea50ce1a33c836fb3f2a0b1568d380b6899c
Martin Smola <smola.martin@gmail.com>
2018-05-31 21:13:54 +0200

    Improved xlstojson

--- xlstojson/xlstojson.js
+++ xlstojson/xlstojson.js
@@ -11,9 +11,20 @@ exports.npm = ['xlsx'];
 
 exports.html = `<div class="padding">
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-6">
 			<div data-jc="textbox" data-jc-path="filename" data-jc-config="placeholder:/path/to/file/name.xls">@(Filename) (@(optional))</div>
 			<div class="help">@(Ignored if a 'buffer' property is in the incomming object.)</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div data-jc="textbox" data-jc-path="sheetname" data-jc-config="placeholder:Sheet1">@(Sheet name) (@(optional, default sheet at index 0))</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div data-jc="textbox" data-jc-path="range" data-jc-config="placeholder:Sheet1">@(Range) (@(optional))</div>
+			<div class="help">@(Use number to set start row or string like A3:B6.)</div>
 		</div>
 	</div>
 </div>`;
@@ -35,12 +46,17 @@ exports.install = function(instance) {
 	instance.custom.process = function(err, buf) {
 
 		var wb = XLSX.read(buf, {type:'buffer'});
-		var ws = wb.Sheets[wb.SheetNames[0]];
+		var ws = wb.Sheets[instance.options.sheetname || wb.SheetNames[0]];
 
-		var arr = XLSX.utils.sheet_to_json(ws);
+		var opts = {};
+
+		var r = instance.options.range;
+		if (r)
+			opts.range = isNaN(r) ? r : parseInt(r);
+
+		var arr = XLSX.utils.sheet_to_json(ws, opts);
 
 		instance.send2(arr);
-
 	};
 
 	instance.on('data', function(flowdata) {
