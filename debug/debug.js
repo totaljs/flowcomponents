e769ea360238932a32496fe9db003d4b9000f2e6
Peter Širka <petersirka@gmail.com>
2018-08-07 00:19:56 +0200

    Added logger.

--- debug/debug.js
+++ debug/debug.js
@@ -5,8 +5,8 @@ exports.color = '#967ADC';
 exports.click = true;
 exports.input = true;
 exports.icon = 'bug';
-exports.version = '2.0.0';
-exports.options = { enabled: true, repository: false, type: 'data' };
+exports.version = '2.0.1';
+exports.options = { enabled: true, repository: false, type: 'data', logger: false };
 exports.readme = `# Debug
 
 Prints data to the debug tab.`;
@@ -18,29 +18,34 @@ exports.html = `<div class="padding">
 			<div data-jc="textbox" data-jc-path="property" data-jc-config="placeholder: @(e.g. address.street)" class="m">@(Path to the property (leave empty to show the whole data object))</div>
 			<div data-jc="textbox" data-jc-path="group" data-jc-config="placeholder: @(e.g. Temperature)" class="m">@(A group name)</div>
 			<div data-jc="checkbox" data-jc-path="enabled">@(Enabled)</div>
+			<div data-jc="checkbox" data-jc-path="logger">@(Log received messages into the file)</div>
 		</div>
 	</div>
 </div>`;
 
 exports.install = function(instance) {
-
 	instance.on('data', function(response) {
 		if (instance.options.enabled) {
+			var val;
 			switch (instance.options.type){
 				case 'both':
 					var data = {};
 					data.repository = response.repository;
 					data.data = response.data;
-					instance.debug(instance.options.property ? U.get(data, instance.options.property) : data, undefined, instance.options.group);
+					val = instance.options.property ? U.get(data, instance.options.property) : data, undefined, instance.options.group;
+					instance.debug(val);
 					break;
 				case 'repository':
-					instance.debug(instance.options.property ? U.get(response.repository, instance.options.property) : response.repository, undefined, instance.options.group);
+					val = instance.options.property ? U.get(response.repository, instance.options.property) : response.repository, undefined, instance.options.group;
+					instance.debug(val);
 					break;
 				case 'data':
 				default:
-					instance.debug(instance.options.property ? U.get(response.data, instance.options.property) : response, undefined, instance.options.group);
+					val = instance.options.property ? U.get(response.data, instance.options.property) : response, undefined, instance.options.group;
+					instance.debug(val);
 					break;
 			}
+			instance.options.logger && LOGGER('flowdebug', instance.name + ':', val.data !== undefined ? val.data : val);
 		}
 	});
 
