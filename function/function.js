5a293c3190be5e8741b7696bd8df10114777d2d3
Peter Širka <petersirka@gmail.com>
2018-09-01 10:26:07 +0200

    Fixed `Function` and send to all output.

--- function/function.js
+++ function/function.js
@@ -73,17 +73,15 @@ exports.install = function(instance) {
 			debug: instance.debug.bind(instance),
 			status: instance.status.bind(instance),
 			send: function(flowdata, index, data){
-
-				if (data === undefined){
-					if (index === undefined)
-						return instance.send2(flowdata.clone());
-					data = index;
-					index = 0;
+				if (data === undefined) {
+					flowdata = flowdata.clone();
+					flowdata.data = index;
+					instance.send2(flowdata);
+				} else {
+					flowdata = flowdata.clone();
+					flowdata.data = data;
+					instance.send2(index, flowdata);
 				}
-
-				flowdata = flowdata.clone();
-				flowdata.data = data;
-				instance.send2(index, flowdata);
 			}
 		},
 		global: {
