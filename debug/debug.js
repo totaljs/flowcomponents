aa44474d3fbdf364ac50ebc2d66b8b71b78bd2b7
Martin Smola <smola.martin@gmail.com>
2019-01-04 10:42:29 +0100

    Improved debug

--- debug/debug.js
+++ debug/debug.js
@@ -5,7 +5,7 @@ exports.color = '#967ADC';
 exports.click = true;
 exports.input = true;
 exports.icon = 'bug';
-exports.version = '2.0.1';
+exports.version = '2.0.2';
 exports.options = { enabled: true, repository: false, type: 'data' };
 exports.readme = `# Debug
 
@@ -36,17 +36,17 @@ exports.install = function(instance) {
 					var data = {};
 					data.repository = rep;
 					data.data = val instanceof Error ? { error: val.message, stack: val.stack } : val;
-					instance.debug(opt.property ? U.get(data, opt.property) : data, undefined, opt.group);
+					instance.debug(safeparse(opt.property ? U.get(data, opt.property) : data), undefined, opt.group);
 					break;
 				case 'repository':
-					instance.debug(opt.property ? U.get(rep, opt.property) : rep, undefined, opt.group);
+					instance.debug(safeparse(opt.property ? U.get(rep, opt.property) : rep), undefined, opt.group);
 					break;
 				case 'data':
 				default:
 					if (val instanceof Error)
 						instance.debug({ error: val.message, stack: val.stack }, undefined, opt.group);
 					else
-						instance.debug(opt.property ? U.get(val, opt.property) : response, undefined, opt.group);
+						instance.debug(safeparse(opt.property ? U.get(val, opt.property) : response), undefined, opt.group);
 					break;
 			}
 		}
@@ -67,4 +67,23 @@ exports.install = function(instance) {
 	};
 
 	instance.custom.status();
+
+	function safeparse(o) {
+		var cache = [];
+		var str = JSON.stringify(o, function(key, value) {
+			if (typeof value === 'object' && value !== null) {
+				if (cache.indexOf(value) !== -1) {
+					try {
+						return JSON.parse(JSON.stringify(value));
+					} catch (error) {
+						return;
+					}
+				}
+				cache.push(value);
+			}
+			return value;
+		});
+		cache = null;
+		return JSON.parse(str);		
+	};
 };
