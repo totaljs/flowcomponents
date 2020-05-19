dc1d77b5f95e5f5bdd1fec80262f47edfe1032aa
Tomas Novak <tomas@tonova.sk>
2020-05-20 00:15:34 +0200

    updated ftpuploadfolder

--- ftpuploadfolder/ftpuploadfolder.js
+++ ftpuploadfolder/ftpuploadfolder.js
@@ -1,5 +1,5 @@
 exports.id = 'ftpuploadfolder';
-exports.version = '1.0.1';
+exports.version = '1.0.2';
 exports.title = 'FTP Upload Folder';
 exports.group = 'FTP';
 exports.color = '#34ace1';
@@ -7,7 +7,7 @@ exports.input = 0;
 exports.output = 1;
 exports.author = 'Total Avengers s.r.o.';
 exports.icon = 'file-upload';
-exports.options = { url: '' };
+exports.options = { type: 'ftp', port: 21, interval: 30000 };
 exports.readme = `# FTP Upload Folder
 
 This component uploads a folder to FTP or SFTP server. __IMPORTANT__: this component uses terminal \`lftp\` command.
@@ -15,20 +15,46 @@ This component uploads a folder to FTP or SFTP server. __IMPORTANT__: this compo
 
 exports.html = `<div class="padding">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="m" data---="textbox__url__required:1;maxlength:500;placeholder:@(E.g. [ftp,sftp]\\://user\\:password@hostname[\\:port][remote path])">@(FTP/SFTP address)</div>
+		<div class="col-md-6">
+			<div data---="dropdown__type__items:FTP|ftp,SFTP|sftp;required:true;__'ftp'" class="m">@(Type)</div>
+		</div>
+		<div class="col-md-6">
+			<div data---="textbox__port__type:number;required:true;placeholder:21" class="m">Port</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-6">
-			<div class="m" data---="textbox__path__required:1;placeholder:/www/upload">@(Local path)</div>
+			<div data---="textbox__hostname__required:true;placeholder:100.100.5.3" class="m">@(Hostname)</div>
 		</div>
 		<div class="col-md-6">
-			<div class="m" data---="textbox__interval__required:1;type:number__5000">@(Interval in ms)</div>
+			<div data---="textbox__interval__required:true;type:number" class="m">@(Interval in ms)</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div class="m" data---="textbox__user__required:true">@(User)</div>
+		</div>
+		<div class="col-md-6">
+			<div class="m" data---="textbox__password__required:true;type:password;placeholder:@(Type a password...)">@(Password)</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div class="m" data---="textbox__remotepath__required:1;placeholder:/">@(Remote path)</div>
+		</div>
+		<div class="col-md-6">
+			<div class="m" data---="textbox__localpath__required:1;placeholder:/www/upload">@(Local path)</div>
 		</div>
 	</div>
 	<div data---="checkbox__isenabled">@(Enable upload)</div>
-</div>`;
+</div>
+<script>
+	WATCH('settings.ftpuploadfolder.type', function(path, value, type) {
+		if (type === 1)
+			return;
+		SET('settings.ftpuploadfolder.port', value === 'ftp' ? 21 : 22);
+	});
+</script>`;
 
 exports.install = function(instance) {
 
@@ -38,18 +64,6 @@ exports.install = function(instance) {
 	var can = false;
 	var isrunning = false;
 	var interval;
-
-	instance.custom.test = function(callback) {
-		Exec(instance.custom.geturl(true), function(err) {
-			if (err)
-				callback && callback(false);
-			else {
-				instance.status('Connected', 'green');
-				callback && callback(true);
-			}
-		});
-	};
-
 
 	instance.custom.create = function() {
 		instance.custom.test(function(working) {
@@ -61,27 +75,69 @@ exports.install = function(instance) {
 		});
 	};
 
-	instance.custom.upload = function() {
-
-		if (isrunning)
-			return;
+	instance.custom.test = function(callback) {
 
 		isrunning = true;
 
-		Exec(instance.custom.geturl(), function(err) {
-			isrunning = false;
-			if (err)
+		Exec('mkdir -p {localpath} && touch {localpath}testfile.txt'.arg(instance.options), function(err) {
+
+			if (err) {
 				instance.throw(err);
-			else
-				instance.send2(SUCCESS(true));
+				return;
+			}
+
+			Exec(instance.custom.geturl(), function(err) {
+				isrunning = false;
+				if (err) {
+					callback && callback(false, err);
+					instance.throw(err);
+					instance.status('Disconnected', 'red');
+				} else {
+					callback && callback(true);
+					instance.send2(SUCCESS(true, 'Test processed successfully!'));
+					instance.status('Connected', 'green');
+				}
+			});
 		});
 	};
 
+	instance.custom.upload = function() {
+
+		isrunning = true;
+
+		U.ls(instance.options.localpath, function(files) {
+
+			if (!files.length) {
+				isrunning = false;
+				return;
+			}
+
+			Exec(instance.custom.geturl(), function(err) {
+				isrunning = false;
+				if (err) {
+					instance.throw(err);
+					instance.status('Disconnected', 'red');
+				} else {
+					instance.send2(SUCCESS(true));
+					instance.status('Connected', 'green');
+				}
+			});
+		});
+	};
+
+	instance.custom.geturl = function() {
+		var opt = instance.options;
+		var builder = [];
+		builder.push('lftp');
+		builder.push('-u {user},{password}'.arg(opt));
+		builder.push('-p ' + opt.port);
+		builder.push('{type}://{hostname}'.arg(opt));
+		builder.push('-e \'mkdir -p {remotepath};mput -c -E -O {remotepath} {localpath}*;exit\''.arg(opt));
+		return builder.join(' ');
+	};
+
 	instance.custom.preparessh = function() {
-
-		var uri = Url.parse(instance.options.url);
-
-		Exec('ssh-keygen -F ' + uri.hostname, function(err) {
+		Exec('ssh-keygen -F ' + instance.options.hostname, function(err) {
 			if (err)
 				instance.custom.generateknownhost();
 			else
@@ -90,10 +146,7 @@ exports.install = function(instance) {
 	};
 
 	instance.custom.generateknownhost = function() {
-
-		var uri = Url.parse(instance.options.url);
-
-		Exec('ssh-keyscan -H {hostname} >> ~/.ssh/known_hosts'.arg(uri), function(err) {
+		Exec('mkdir -p ~/.ssh/ && ssh-keyscan -H {hostname} >> ~/.ssh/known_hosts'.arg(instance.options), function(err) {
 			if (err)
 				instance.throw(err);
 			else
@@ -101,53 +154,48 @@ exports.install = function(instance) {
 		});
 	};
 
-	instance.custom.geturl = function(istest) {
-		var opt = instance.options;
-		var uri = Url.parse(opt.url);
-
-		var builder = [];
-		var localpath = opt.path.substr(-1) === '/' ? opt.path : opt.path + '/';
-
-		builder.push('lftp');
-
-		if (uri.auth) {
-			uri.auth = uri.auth.split(':');
-			builder.push('-u {0},{1}'.format(uri.auth[0], uri.auth[1]));
-		}
-
-		if (uri.port)
-			builder.push('-p ' + uri.port);
-
-		builder.push('{0}//{1}{2}'.format(uri.protocol, uri.hostname));
-
-		if (istest) {
-			builder.push('-e \'ls;exit\'');
-		} else {
-			if (uri.path)
-				builder.push('-e \'mkdir -p {0};cd {0};lcd {1};mirror -R -c --Remove-source-files;exit\''.format(uri.path, localpath));
-			else
-				builder.push('-e \'lcd {0};mirror -R -c --Remove-source-files;exit\''.format(localpath));
-		}
-
-		return builder.join(' ');
-	};
-
-	instance.close = function() {
+	instance.close = function(next) {
 		isrunning = false;
-		clearInterval(interval);
+		interval && clearInterval(interval);
+		next();
     };
 
-	instance.reconfigure = function() {
+	instance.reconfigure = function(opt) {
+
 		interval && clearInterval(interval);
-		can = instance.options.url && instance.options.path && instance.options.interval && instance.options.isenabled ? true : false;
+
+		var local = instance.options.localpath;
+		var remote = instance.options.remotepath;
+
+		if (instance.options.localpath) {
+			instance.options.localpath = instance.options.localpath[0] === '/' ? instance.options.localpath : '/' + instance.options.localpath;
+			instance.options.localpath = instance.options.localpath[instance.options.localpath.length - 1] === '/' ? instance.options.localpath : instance.options.localpath + '/';
+		} else
+			instance.options.localpath = '/';
+
+		if (instance.options.remotepath) {
+			instance.options.remotepath = instance.options.remotepath[0] === '/' ? instance.options.remotepath : '/' + instance.options.remotepath;
+			instance.options.remotepath = instance.options.remotepath[instance.options.remotepath.length - 1] === '/' ? instance.options.remotepath : instance.options.remotepath + '/';
+		} else
+			instance.options.remotepath = '/';
+
+		if (local !== instance.options.localpath || remote !== instance.options.remotepath) {
+			instance.reoptions(instance.options);
+			return;
+		}
+
+		can = instance.options.type && instance.options.port && instance.options.hostname && instance.options.interval && instance.options.user && instance.options.password ? true : false;
 		instance.status(can ? '' : 'Not configured', can ? undefined : 'red');
 
 		if (!can)
 			return;
 
-		instance.status('Disconnected', 'red');
+		if (!instance.options.isenabled) {
+			instance.status('Disabled', 'orange');
+			return;
+		}
 
-		if (instance.options.url.indexOf('sftp') !== -1)
+		if (instance.options.type === 'sftp')
 			instance.custom.preparessh();
 		else
 			instance.custom.create();
